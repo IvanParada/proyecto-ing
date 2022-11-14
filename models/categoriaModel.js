ba92019c08bebe367d9566204669930299eb2052
Kok-e <118061358+Kok-e@users.noreply.github.com>
2022-11-13 21:00:38 -0300

    Se define la constante espacios comunes

---
+++ models/categoriaModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CategoriaSchema = new Schema({
+    tipo:{
+        type: String,
+        required: true
+    },
+    aforo:{
+        type: Number,
+        required: true,
+    },
+    disponibilidad:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('categoriaModel',CategoriaSchema);
