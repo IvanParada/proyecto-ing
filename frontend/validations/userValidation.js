5cddd28468d4a215f310807d248f321f55b9c650
IvanParada <118088453+IvanParada@users.noreply.github.com>
2023-01-01 22:27:00 -0300

    Add files via upload

--- frontend/validations/userValidation.js
+++ frontend/validations/userValidation.js
@@ -1,22 +1,23 @@
 import * as yup from 'yup'
 
+
 const userValidation = yup.object({
     nombres: yup.string()
     .required("Nombres obligatorios")
-    .matches(/^[a-zA-Z ]*$/, "Nombres no puede contener caracteres especiales"),
+    .matches(/^[a-zA-Z\s]*$/, "Nombres no puede contener caracteres especiales"),
     apellidos: yup.string()
     .required("Apellidos obligatorios")
-    .matches(/^[a-zA-Z ]*$/, "Apellidos no puede contener caracteres especiales"),
+    .matches(/^[a-zA-Z\s]*$/, "Apellidos no puede contener caracteres especiales"),
     rut: yup.string()
-    .matches(/^[kK0-9 ]*$/, "El RUT no puede contener caracteres especiales")
+    .matches(/^[0-9]*$/, "El RUT solo debe contener números")
     .min(8,"El rut no es válido")
     .max(9,"El rut no es válido"),
     estado: yup.string()
     .required("Estado obligatorio")
-    .matches(/^[a-zA-Z]*$/, "Nombres no puede contener caracteres especiales, ni números"),
+    .matches(/^[a-zA-Z]*$/, ""),
     tipoUsuario: yup.string()
     .required("Tipo de usuario obligatorio")
-    .matches(/^[a-zA-Z]*$/, "Nombres no puede contener caracteres especiales, ni números"),
+    .matches(/^[a-zA-Z]*$/, ""),
 })
 
 export default userValidation
